7918eb8919f2f4189e1b2d37e004e30e0ef48825
tpracticeaccount <tusharw20@gmail.com>
2022-01-31 15:43:04 +0530

    Loopd code

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "ec2_example" {
+        count = 3
 
    ami           = "ami-03fa4afc89e4a8a09"
 
    instance_type =  var.instance_type
 
    tags = {
-           Name = var.environment_name
+           Name = "Terraform EC2-${count.index}"
    }
 }
+
